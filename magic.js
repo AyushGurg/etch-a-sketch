f2eb38fe1f8ee1223f25f865c74a2f4e048dd8a4
AyushGurg <ayshgrg@gmail.com>
2023-01-25 17:27:27 -0700

    made a line of pixel which can be modify the number of pixel.

--- magic.js
+++ magic.js
@@ -1,14 +1,20 @@
 
-let gridsize = 16;/*prompt("what size grid? (Max 64");*/
+let gridsize = 30;/*prompt("what size grid? (Max 64");*/
 
-    
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.textContent = "h";
-        document.getElementById('sketchpad').appendChild(pixel);
-        
+//getting the dimensions of the sketchpad
+let sketchpad = document.getElementById('sketchpad');
+let sketchpadWidth = window.getComputedStyle(sketchpad).getPropertyValue('width');
 
-        
-        pixel.addEventListener("mouseenter",() => {
-            pixel.style.backgroundColor = "red";
-        })
+let pixelWidth = (parseInt(sketchpadWidth) / gridsize) + 'px';
+
+for (let height = 0; height < gridsize; height++) {
+    const pixel = document.createElement('div');
+    pixel.classList.add('pixel');    
+    sketchpad.appendChild(pixel);
+    pixel.style.height = pixelWidth;
+    pixel.style.flexBasis = pixelWidth;
+    pixel.addEventListener("mouseenter",() => {
+    pixel.style.backgroundColor = "red";
+    });
+  
+}
