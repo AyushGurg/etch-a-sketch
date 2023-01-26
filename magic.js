9cd4f22b5a0ead985fda441f765f1c52d76e2882
AyushGurg <ayshgrg@gmail.com>
2023-01-26 10:16:02 -0700

    Change the line of pixel to grid which is dynamic which can change with given grid size

--- magic.js
+++ magic.js
@@ -1,20 +1,39 @@
-
-let gridsize = 30;/*prompt("what size grid? (Max 64");*/
-
-//getting the dimensions of the sketchpad
 let sketchpad = document.getElementById('sketchpad');
 let sketchpadWidth = window.getComputedStyle(sketchpad).getPropertyValue('width');
+let pixelWidth = 0;
 
-let pixelWidth = (parseInt(sketchpadWidth) / gridsize) + 'px';
+// Default size
+let gridsize = 1;
+makegrid();
 
-for (let height = 0; height < gridsize; height++) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel');    
-    sketchpad.appendChild(pixel);
-    pixel.style.height = pixelWidth;
-    pixel.style.flexBasis = pixelWidth;
-    pixel.addEventListener("mouseenter",() => {
-    pixel.style.backgroundColor = "red";
-    });
-  
+
+
+//
+let gridsizeChange = document.getElementById('gridsize');
+gridsizeChange.addEventListener('click', () => {
+    gridsize = prompt("How many pixels on the sketchpad?");
+    makegrid();
+})
+
+//getting the dimensions of the sketchpad
+function makegrid() {
+    
+    //Clear the sketchpad
+    sketchpad.innerHTML = "";
+    
+    // Makes the grid
+    pixelWidth = (parseInt(sketchpadWidth) / gridsize) + 'px';
+
+    for (let height = 0; height < gridsize; height++) {
+        for (let width = 0; width < gridsize; width++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');    
+            sketchpad.appendChild(pixel);
+            pixel.style.height = pixelWidth;
+            pixel.style.flexBasis = pixelWidth;
+            pixel.addEventListener("mouseenter",() => {
+            pixel.style.backgroundColor = "white";
+            });
+        }
+    } 
 }
